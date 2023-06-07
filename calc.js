f0fbd5f6187963e94c11091b21b43593cf933f3f
ksha <kesha.kevadiya18@gmail.com>
2023-06-07 09:52:06 +0530

    calc

---
+++ calc.js
@@ -0,0 +1,11 @@
+function insert(num) {
+  document.form1.textview.value += num;
+}
+function equal() {
+  var exp = document.form1.textview.value;
+  var calc = eval(exp);
+  document.form1.textview.value = calc;
+}
+function backspace() {
+  document.form1.textview.value = "";
+}
